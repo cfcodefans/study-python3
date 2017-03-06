b6ac8c22c77f8aea25264d03957886ed0eb5b061
cfcodefans <cf.chenfan@gmail.com>
2017-03-06 09:50:23 +0800

    ..

--- src/informal_intro/CalculatorTests.py
+++ src/informal_intro/CalculatorTests.py
@@ -1,8 +1,19 @@
 import unittest
 
-class CalculatorTests (unittest.TestCase):
+
+class CalculatorTests(unittest.TestCase):
     def test_math(self):
         self.assertEqual(2 + 2, 4, "2 + 2 = 4")
+        self.assertEqual(50 - 5 * 6, 20, "50 - 5 * 6 = 20")
+        self.assertEqual((50 - 5 * 6) / 4, 5, "(50 - 5 * 6) / 4 = 5")
+        self.assertEqual(8 / 5, 1.6, "8 / 5 = 1.6")
+        self.assertAlmostEqual(17 / 3, 5.66, 1, "17 / 3 = 5.66")
+        self.assertEqual(17 // 3, 5, "17 // 3 = 5")
+        self.assertEqual(17 % 3, 2, "17 % 3 = 2")
+        self.assertEqual(17 % -3, -1, "17 % -3 = -1")
+        self.assertEqual(-17 % 3, 1, "-17 % 3 = 1")
+        self.assertEqual(-17 % -3, -2, "-17 % -3 = -2")
+
 
 if __name__ == '__main__':
     unittest.main()
