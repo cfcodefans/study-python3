928a65a9742cec78e696f2471ea73e8a1d84b383
cfcodefans <cf.chenfan@gmail.com>
2017-07-06 21:51:03 +0800

    ..

--- src/study/builtins/ListTests.py
+++ src/study/builtins/ListTests.py
@@ -2,8 +2,15 @@ import unittest
 
 
 class ListTests(unittest.TestCase):
+    def setUp(self):
+        print()
+
+    def test_repeat(self):
+        print([1] * 5)
+
+
     def test_delete(self):
-        _list:list = list(range(3, 9))
+        _list: list = list(range(3, 9))
         print(_list)
         _list.remove(5)
         print(_list)
@@ -13,6 +20,20 @@ class ListTests(unittest.TestCase):
         print(_list)
         _list.sort()
         print(_list)
+
+    def test_index(self):
+        _list: list = list(range(0, 10))
+        print(_list)
+        print(_list[5])
+        print(_list[5:])
+        print(_list[:5])
+        print(_list[-3])
+        print(_list[-3:])
+        print(_list[:-3])
+
+    def test_add(self):
+        print([2] + [4])
+        print([2].append(4))
 
 if __name__ == '__main__':
     unittest.main
